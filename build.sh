36321ca25d676d19494a06fc6cb839612fa3c376
Laurent Thévenoux <lrnt@thvnx.com>
2019-11-14 21:11:32 +0100

    Fix config command

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cp CMakeLists.txt ClangFCO.cpp llvm-project/clang-tools-extra/clang-fco
 mkdir llvm-project-build
 cd llvm-project-build
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=../llvm-project-root \
       -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_BUILD_LLVM_DYLIB=True \
-      -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -G "Ninja" ../llvm-project/llvm
+      -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" ../llvm-project/llvm
 ninja
